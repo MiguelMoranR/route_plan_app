e303ac9ecdacf75c8ba51ef8d59bb2a9e60db1e3
Samsung_Pc <miguel381@gmail.com>
2024-04-14 22:23:46 -0500

    Actualización

--- app/src/main/java/co/edu/uan/route_plan_app/MainActivity.kt
+++ app/src/main/java/co/edu/uan/route_plan_app/MainActivity.kt
@@ -3,17 +3,26 @@ package co.edu.uan.route_plan_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import co.edu.uan.route_plan_app.ui.theme.Route_plan_appTheme
+import androidx.compose.foundation.Image
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+       override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Route_plan_appTheme {
@@ -22,7 +31,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Aplica ruta de transporte")
+                    GreetingImage(
+                        message = "Bienvenido...",
+                        ingreso = "Ingrese sus credenciales")
                 }
             }
         }
@@ -30,17 +41,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "App $name!",
+fun GreetingImage(message: String, ingreso: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.fondo_app)
+
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
+    ) {
+        Text(
+        text = message,
+        fontSize = 40.sp,
+        modifier = Modifier
+            .padding(16.dp)
+            .align(alignment = Alignment.CenterHorizontally)
     )
+    Text(
+        text = ingreso,
+        fontSize = 30.sp,
+        modifier = Modifier
+            .padding(16.dp)
+            .align(alignment = Alignment.CenterHorizontally)
+
+        )
+
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     Route_plan_appTheme {
-        Greeting("Aplicación ruta de transporte")
+        GreetingImage(message = "Bienvenido...", ingreso = "Ingrese sus credenciales")
     }
 }
