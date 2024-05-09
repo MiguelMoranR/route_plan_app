68f75a3ed035b4768b762e3ad76a140017eab699
Samsung_Pc <miguel381@gmail.com>
2024-05-08 23:00:00 -0500

    Actualización

--- app/src/androidTest/java/co/edu/uan/route_plan_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/co/edu/uan/route_plan_app/ExampleInstrumentedTest.kt
@@ -1,24 +1,18 @@
 package co.edu.uan.route_plan_app
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleUnitTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("co.edu.uan.route_plan_app", appContext.packageName)
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
